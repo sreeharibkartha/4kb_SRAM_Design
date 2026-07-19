1945da079fae677f496cc7874a3aa435b53f3e92
sreeharibkartha <sreeharibec51@gmail.com>
2026-07-19 16:12:38 +0530

    Delete week5_final directory

--- week5_final/drc/extract_run.tcl
+++
@@ -1,6 +0,0 @@
-load cell6t
-select top cell
-extract all
-ext2spice lvs
-ext2spice -o ~/week5_final/lvs/cell6t_extracted.sp
-quit -noprompt
--- week5_final/lvs/lvs_setup.tcl
+++
@@ -1,1 +0,0 @@
-lvs "cell6t_extracted.sp cell6t" "cell6t_golden.sp cell6t" $env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs_report.txt
